7b66cec1cc7774bba018043a5f464b3c38b9f9a5
lbp1122 <lbp112@gmail.com>
2026-09-04 12:33:38 +0800

    Bump Android app to 2.3.7

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lbp.balancesheetsquare"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
-        versionName = "2.3.6"
+        versionCode = 14
+        versionName = "2.3.7"
         buildConfigField("boolean", "DIRECT_PAID", directPaid.toString())
         resValue("string", "app_name", "Balance Sheet Square")
     }
